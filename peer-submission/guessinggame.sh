e32793977c962187264344c39ded11fa5b22ef1e
priyammathur <priyam_mathur92@yahoo.co.in>
2019-05-15 19:30:06 +0530

    GuessingGame updated

--- peer-submission/guessinggame.sh
+++ peer-submission/guessinggame.sh
@@ -1,17 +1,23 @@
-#! /usr/bin/env bash
 filenumbers=$(ls | wc -l)
-echo "guess the number of files in the current directory"
-read answer
-function count_files {
-	if [[ $answer -eq $filenumbers ]]
-	then
-		echo "Correct number of files"
-	elif [[ $answer -gt $filneumbers ]]
-	then
-		echo "Your guess is greater than the actual number of files"
-	else
-		echo "Your guess is less than the actual number of files"
-	fi
+
+function count_files() {
+	echo "Enter the number of files in the current directory"
+	while read answer
+	do
+
+		if [[ $answer -eq $filenumbers ]]
+		then
+			echo "Congrats! Correct number of files"
+			exit
+		elif [[ $answer -gt $filneumbers ]]
+		then
+			echo "Your guess is greater than the actual number of files"
+			exit
+		else
+			echo "Your guess is less than the actual number of files"
+			exit
+		fi
+	done
 }
 
 count_files
